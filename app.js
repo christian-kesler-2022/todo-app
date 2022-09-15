e51b58156b320d1dc51e73ae40b13e1cb120b080
christian-kesler-2022 <111981585+christian-kesler-2022@users.noreply.github.com>
2022-09-15 09:01:44 -0700

    Functional demo

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-console.log(`Hello Node.js v${process.versions.node}!`);
-
 var express = require('express');
 var todoController = require('./controllers/todoController.js');
 
 var app = express();
 
+// setup template engine
 app.set('view engine', 'ejs');
 
+// static files
 app.use(express.static('./public'));
+// app.use('/assets', express.static('./public'));
 
+// fire controllers
 todoController(app);
 
+// listen to port
 app.listen(3000);
-console.log('You are listening to port 3000');
+console.log('Server is listening to 3000');
